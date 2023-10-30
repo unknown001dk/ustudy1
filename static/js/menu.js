a6331b00023a75c50802f21a53234558793395f2
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2023-10-30 20:59:18 +0530

    Add files via upload

---
+++ static/js/menu.js
@@ -0,0 +1,11 @@
+/*var req = unirest("GET", "https://www.universal-tutorial.com/api/getaccesstoken");
+
+  req.headers({
+    "Accept": "application/json",
+    "api-token": "87j7zwhNbMAEwCgqinTvK6Jpvlvh6tHMqr2MH0Do6nmczRj-lUmxkB2czwVXJ_UoD_k",
+    "user-email": "dk9232525@gmail.com"
+  });*/
+
+  function onToggleMenu(e){
+    e.name = "close"
+  }
