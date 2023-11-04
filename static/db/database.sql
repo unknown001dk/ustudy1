efaca74c52bbed14ba1abcd7f1b96893700b29e9
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2023-11-04 20:13:55 +0530

    database added

--- static/db/database.sql
+++ static/db/database.sql
@@ -26,4 +26,22 @@ INSERT INTO `register`(id, name, email, phonenumber, password)
 INSERT INTO `register`(id, name, email, phonenumber, password)
           VALUES (4, 'rohith', 'dk9232525@gmail.com', 9361318372, 'ad123ad1');
 
+
+CREATE TABLE `htmlregister`(
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,`name` VARCHAR(25) NOT NULL
+  ,`email` VARCHAR(25) NOT NULL
+  ,`password` VARCHAR(10) NOT NULL
+  ,`gender`   VARCHAR(10) NOT NULL                -- m-male  fe-female  ot-others
+  ,`degree`   VARCHAR(30) NOT NULL                -- ec-enginnering collage   ar-arts   ot-others
+  ,`whichyear` VARCHAR(10) NOT NULL              -- 1y-1st year   2y-2nd year    3y-3rd year  fi-final year ot-others
+  ,`laptopavailable` VARCHAR(10) NOT NULL        -- y-yes   n-no
+  ,`availabletime` VARCHAR(10) NOT NULL          -- 6-6.30-7.30    7-7.00-8.00   8-8.30-9.30
+  ,`whychoose`  VARCHAR(200) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `htmlregister`(id, name, email, password, gender, degree, whichyear, laptopavailable, availabletime, whychoose)
+                      VALUES(1, 'dinesh', 'dk123@gmail.com', 'ad123ad1', 'male', 'others', 'final year', 'yes', '6.30-7.30', 'I like your course that i choose it');
+
+
 COMMIT;
