063e0c77a616100b61a874a3fe742cccf44818b9
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2023-11-16 19:14:12 +0530

    new

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,1 +1,14 @@
-const 
+const modal = document.querySelector('.modal');
+const showmodel = document.querySelector('.show-modal');
+const closemodal = document.querySelectorAll('.close-modal');
+
+showmodel.addEventListener('click', function(){
+  modal.classList.remove('hidden')
+});
+
+closemodal.forEach(close =>{
+  close.addEventListener('click',function(){
+    modal.classList.add('hidden')
+  });
+});
+
